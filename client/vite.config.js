1db9335e0d7d5016e8fe71457dc9dcc8a837c18c
Mahmoud-rifaat <mahmoud.ref2at.dev@gmail.com>
2024-02-10 08:41:21 +0200

    create proxy and signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //We added a server proxy to replace each occurrence of '/api' with our localhost server
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+
+    },
+  },
   plugins: [react()],
 })
